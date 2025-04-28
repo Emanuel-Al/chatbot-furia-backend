391aefa09c80e3da6da615d4e444b5b2aa000aeb
Emanuel Reino <emanuelreino13@gmail.com>
2025-04-28 19:42:35 -0300

    feat: updated chatRoutes in server

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get("/", (req, res) => {
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-const chatRoutes = require("./api/routes/chat");
+const chatRoutes = require("./api/routes/chatRoutes");
 const userRoutes = require("./api/routes/userRoutes");
 const { connect } = require("mongoose");
-app.use("/chat", chatRoutes);
+app.use("/", chatRoutes);
 app.use("/", userRoutes);
 app.use(passport.initialize());
 
