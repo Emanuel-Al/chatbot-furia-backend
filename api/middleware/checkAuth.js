dfbda92bceb93aef3a0f166a7e98fca8c9a30894
Emanuel Reino <emanuelreino13@gmail.com>
2025-04-28 15:10:26 -0300

    feat: checkAuth middleware

---
+++ api/middleware/checkAuth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function checkAuth(req, res, next) {
+  const token =
+    req.headers["authorization"] && req.headers["authorization"].split("")[1];
+  if (!token) {
+    return res.status(401).json({ message: "token não encontrado" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    if (error) {
+      return res.status(403).json({ message: "Token inválido" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = { checkAuth };
