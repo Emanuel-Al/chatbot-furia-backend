a0b2bc93b4cf6dc691ee40fc50bfc454ae61b711
Emanuel Reino <emanuelreino13@gmail.com>
2025-04-28 15:08:24 -0300

    feat: passport init

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const connectDb = require("./config/db");
 const app = require("./config/express")();
 const port = app.get("port");
+const passport = require("./config/passport");
 
 connectDb();
 
@@ -16,5 +17,6 @@ const userRoutes = require("./api/routes/userRoutes");
 const { connect } = require("mongoose");
 app.use("/chat", chatRoutes);
 app.use("/", userRoutes);
+app.use(passport.initialize());
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
