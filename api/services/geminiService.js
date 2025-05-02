2aa60bde0b06863526a59b80db784862a2c096d3
Emanuel Reino <emanuelreino13@gmail.com>
2025-05-01 23:47:46 -0300

    feat: changed response format

--- api/services/geminiService.js
+++ api/services/geminiService.js
@@ -7,13 +7,17 @@ async function askGemini(prompt) {
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
-      contents: prompt,
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: prompt }],
+        },
+      ],
     });
     return response.candidates[0].content.parts[0].text;
   } catch (error) {
-    console.log("Erro ao gerar responta", error.message);
+    console.log("Erro ao gerar resposta", error.message);
     throw new Error("Erro ao gerar resposta");
   }
 }
-
 module.exports = { askGemini };
