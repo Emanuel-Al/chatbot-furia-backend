951cca5ee4a346b3c9cc517229afd6ab72213001
Emanuel Reino <emanuelreino13@gmail.com>
2025-05-02 14:36:27 -0300

    feat: gemini service updated to use a base knowledge about furia

--- api/services/geminiService.js
+++ api/services/geminiService.js
@@ -1,23 +1,28 @@
 const genai = require("@google/genai");
+const { getInitialContext } = require("./knowledgeService");
 
 const apiKey = process.env.GEMINI_API_KEY;
 const ai = new genai.GoogleGenAI({ apiKey: `${apiKey}` });
 
-async function askGemini(prompt) {
+async function askGemini(userPrompt) {
+  const context = getInitialContext();
+
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
       contents: [
         {
           role: "user",
-          parts: [{ text: prompt }],
+          parts: [{ text: context }, { text: userPrompt }],
         },
       ],
     });
+
     return response.candidates[0].content.parts[0].text;
   } catch (error) {
-    console.log("Erro ao gerar resposta", error.message);
+    console.error("Erro ao gerar resposta", error.message);
     throw new Error("Erro ao gerar resposta");
   }
 }
+
 module.exports = { askGemini };
